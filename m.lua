e439670bb0ff3600a632caa030a58bff700f6935
AppleGam1ng-ax <84991912+AppleGam1ng-ax@users.noreply.github.com>
2024-11-30 21:17:47 +0100

    Update m.lua

--- m.lua
+++ m.lua
@@ -1,5 +1,6 @@
 if not getgenv().PandaLoaded == true then
     game.Players.LocalPlayer:Kick("[Fullyz.xyz] Panda has not finished loading.")
 else
-    print("[Fullyz.xyz] Panda is properly loaded.")
+    print("[Fullyz.xyz] Library is properly loaded.")
+    print("[Fullyz.xyz] Inizialized script...")
 end
